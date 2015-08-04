2cbd87fadc31209bc8f94fa91b160eb6473a490f
jcarapet <jeffcarapetyan@gmail.com>
2015-08-04 12:19:48 -0500

    reset to localhost for standalone start

--- cookbooks/elkstack/attributes/topology.rb
+++ cookbooks/elkstack/attributes/topology.rb
@@ -7,21 +7,3 @@
 # Copyright 2015, YOUR_COMPANY_NAME
 #
 
-# Attributes for specific node_type
-if node['topo']['node_type'] == "elkstackStandalone"
-  default['df_elasticsearch']['network_host'] = "localhost" 
-  default['df_kibana']['kibana_version'] = "kibana-4.0.1-linux-x64" 
-  default['df_kibana']['initscript'] = "https://gist.githubusercontent.com/thisismitch/8b15ac909aed214ad04a/raw/bce61d85643c2dcdfbc2728c55a41dab444dca20/kibana4" 
-  default['df_kibana']['kibana_host'] = "localhost" 
-  default['df_kibana']['fqdn'] = "dfkibana.com" 
-  default['df_logstash']['logstash_ip'] = "10.0.1.2" 
-  default['df_logstash']['logstash_port'] = "5000" 
-  default['df_logstash']['ssl_path'] = "/etc/pki/tls" 
-  default['df_logstash']['elasticsearch_host'] = "localhost" 
-  override['java']['jdk_version'] = 8 
-  override['java']['install_flavor'] = "oracle" 
-  override['java']['oracle']['accept_oracle_download_terms'] = true 
-  override['java']['version'] = "7" 
-  override['nginx']['version'] = "1.4.2" 
-  override['elasticsearch']['version'] = "1.4.5" 
-end
